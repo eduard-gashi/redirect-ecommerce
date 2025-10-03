ac64544bb98077d0f8f4b97d7506b877057346b4
Eduard Gashi <gashi.eduard@gmx.de>
2025-10-03 13:45:51 +0200

    Improve CSS aesthetics

--- client/src/pages/Products.jsx
+++ client/src/pages/Products.jsx
@@ -28,7 +28,7 @@ function Products() {
             
             {/* Header */}
             <div className="product-header-container">
-                <h1 className="title logo-color">Unsere Produkte</h1>
+                <h1 className="title-black logo-color">Unsere Produkte</h1>
                 <p className="text-paragraph">Entdecken Sie unsere Kollektion.</p>
             </div>
 
