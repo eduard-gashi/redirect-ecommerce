67012acabf782f26bdb40913c978c28e5f380924
Eduard Gashi <gashi.eduard@gmx.de>
2025-10-06 13:46:12 +0200

    Track order in Google Analytics

--- client/src/pages/Checkout.jsx
+++ client/src/pages/Checkout.jsx
@@ -9,13 +9,23 @@ const PAYPAL_CLIENT_ID = import.meta.env.VITE_PAYPAL_LIVE_CLIENT_ID;
 function CheckoutScreen() {
   const { id } = useParams();
   const navigate = useNavigate();
-  const location = useLocation(); 
+  const location = useLocation();
 
   const defaultQuantity = location.state?.checkoutQuantity || 1;
 
   const [product, setProduct] = useState(null);
-  const [quantity, setQuantity] = useState(defaultQuantity); 
-  const { addToCart, clearCart } = useContext(CartContext); 
+  const [quantity, setQuantity] = useState(defaultQuantity);
+  const { addToCart, clearCart } = useContext(CartContext);
+
+  // Google Analytics tag
+  const gtag = (...args) => {
+    if (typeof window.gtag === 'function') {
+      window.gtag(...args);
+    } else {
+      console.warn("GA: gtag function not found (Is the script loaded?).");
+    }
+  };
+
 
   useEffect(() => {
     const fetchProduct = async () => {
@@ -26,12 +36,12 @@ function CheckoutScreen() {
         console.error("Error while loading the product:", err);
       }
     };
-    if (id) { 
+    if (id) {
       console.log("Fetching product with ID:", id);
-       fetchProduct();
-      } else {
-        console.warn("Product ID is missing in URL parameters.");
-      }
+      fetchProduct();
+    } else {
+      console.warn("Product ID is missing in URL parameters.");
+    }
   }, [id]);
 
   if (!product) {
@@ -67,33 +77,52 @@ function CheckoutScreen() {
   const onApprove = async (data, actions) => {
     // 2. Payment approved by user, get details (address etc.)
     const details = await actions.order.capture();
-    
+
     const shippingAddress = {
-        name: details.payer.name.given_name + ' ' + details.payer.name.surname,
-        address: details.purchase_units[0].shipping.address.address_line_1,
-        city: details.purchase_units[0].shipping.address.admin_area_2,
-        postalCode: details.purchase_units[0].shipping.address.postal_code,
+      name: details.payer.name.given_name + ' ' + details.payer.name.surname,
+      address: details.purchase_units[0].shipping.address.address_line_1,
+      city: details.purchase_units[0].shipping.address.admin_area_2,
+      postalCode: details.purchase_units[0].shipping.address.postal_code,
     };
-    
+
     // 3. Save order in backend and clear cart
     if (details.status === 'COMPLETED') {
       const orderData = {
-          orderItems: [{ ...product, quantity: quantity }], 
-          shippingAddress: shippingAddress,
-          paymentMethod: 'PayPal Express',
-          totalPrice: product.price * quantity,
-          paymentResult: { id: details.id, status: details.status },
+        orderItems: [{ ...product, quantity: quantity }],
+        shippingAddress: shippingAddress,
+        paymentMethod: 'PayPal Express',
+        totalPrice: product.price * quantity,
+        paymentResult: { id: details.id, status: details.status },
       };
 
       try {
-          const orderRes = await apiClient.post('/orders', orderData); 
-          
-          if (orderRes.status === 201) {
-              clearCart();
-              navigate(`/order/${orderRes.data._id}`);
-          }
+        const orderRes = await apiClient.post('/orders', orderData);
+
+        if (orderRes.status === 201) {
+
+          // Track Order in google Analytics
+          gtag('event', 'purchase', {
+            transaction_id: orderRes.data._id,
+            affiliation: 'redirect',
+            value: orderData.totalPrice,
+            currency: 'EUR',
+            shipping: 0.00,
+            items: [
+              {
+                item_id: product._id,
+                item_name: product.name,
+                currency: 'EUR',
+                price: product.price,
+                quantity: quantity
+              }
+            ]
+          });
+
+          clearCart();
+          navigate(`/order/${orderRes.data._id}`);
+        }
       } catch (err) {
-          console.error("Fehler beim Speichern der Express-Bestellung:", err);
+        console.error("Fehler beim Speichern der Express-Bestellung:", err);
       }
     }
   };
@@ -107,7 +136,7 @@ function CheckoutScreen() {
     <div className="bg-gray-100 min-h-screen flex flex-col justify-between">
       <div className="container mx-auto px-4 py-12">
         <div className="bg-white hover-product border rounded-lg shadow-lg p-8 max-w-4xl mx-auto flex flex-row gap-10 transition-transform">
-          
+
           {/* Image Box */}
           <div className="flex-1 flex justify-center">
             <img
@@ -120,7 +149,7 @@ function CheckoutScreen() {
           {/* Details & Cart */}
           <div className="flex-1">
             <h1 className="text-3xl font-bold mb-4">{product.name}</h1>
-            
+
             {/* Price and Description */}
             <p className="text-xl font-semibold mb-4">€{product.price}</p>
 
@@ -132,28 +161,28 @@ function CheckoutScreen() {
             </p>
 
             <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
-                
-                {/* 1. Regular cart checkout */}
-                <button
-                    onClick={() => addToCart(product, quantity)}
-                    disabled={product.countInStock === 0}
-                    className="add-to-cart-button" 
-                >
-                    🛒 In den Warenkorb
-                </button>
 
-                {/* 2. PayPal Checkout */}
-                <div style={{ marginTop: '10px' }}>
-                    <PayPalScriptProvider options={{ clientId: PAYPAL_CLIENT_ID, currency: "EUR" }}>
-                        <PayPalButtons
-                            style={{ layout: "vertical", color: "gold", shape: "rect", label: "paypal", tagline: false }}
-                            createOrder={createOrder}
-                            onApprove={onApprove}
-                            onCancel={onCancel}
-                            disabled={product.countInStock === 0}
-                        />
-                    </PayPalScriptProvider>
-                </div>
+              {/* 1. Regular cart checkout */}
+              <button
+                onClick={() => addToCart(product, quantity)}
+                disabled={product.countInStock === 0}
+                className="add-to-cart-button"
+              >
+                🛒 In den Warenkorb
+              </button>
+
+              {/* 2. PayPal Checkout */}
+              <div style={{ marginTop: '10px' }}>
+                <PayPalScriptProvider options={{ clientId: PAYPAL_CLIENT_ID, currency: "EUR" }}>
+                  <PayPalButtons
+                    style={{ layout: "vertical", color: "gold", shape: "rect", label: "paypal", tagline: false }}
+                    createOrder={createOrder}
+                    onApprove={onApprove}
+                    onCancel={onCancel}
+                    disabled={product.countInStock === 0}
+                  />
+                </PayPalScriptProvider>
+              </div>
             </div>
           </div>
         </div>
