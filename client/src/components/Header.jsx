5ff7b16f0b57dcb191d2f33de8df9fdc92b48419
Eduard Gashi <gashi.eduard@gmx.de>
2025-11-16 20:30:11 +0100

    Improve decoration for mobile device

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,15 +1,27 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { CartContext } from "../context/CartContext";
 import "../App.css";
-import { FaUserCircle } from 'react-icons/fa';
+import { FaUserCircle, FaBars, FaTimes } from 'react-icons/fa';
+
+
 function Header() {
   const { cartItems } = useContext(CartContext);
   const totalItems = cartItems.reduce((acc, item) => acc + item.quantity, 0);
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
     <header className="main-header">
       <div className="header-container">
+
+        {/* Hamburger */}
+        <button
+          className="mobile-menu-button"
+          onClick={() => setMobileMenuOpen(true)}
+        >
+          <FaBars size={28} />
+        </button>
+
         {/* Logo */}
         <Link to="/" className="header-logo-link">
           <img
@@ -51,6 +63,24 @@ function Header() {
           </Link>
         </nav>
       </div>
+
+      {/* Mobile Overlay Menu */}
+      {mobileMenuOpen && (
+        <div className="mobile-menu-overlay">
+          <button
+            className="close-menu-button"
+            onClick={() => setMobileMenuOpen(false)}
+          >
+            <FaTimes size={24} />
+          </button>
+          <nav className="mobile-nav">
+            <Link to="/kontakt" className="nav-link" onClick={() => setMobileMenuOpen(false)}>Kontakt</Link>
+            <br></br>
+            <Link to="/products" className="nav-link" onClick={() => setMobileMenuOpen(false)}>Produkte</Link>
+          </nav>
+        </div>
+      )}
+
     </header>
   );
 }
