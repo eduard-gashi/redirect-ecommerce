d00b6bc66d43ddd9402b4691476131b481c4004e
Eduard Gashi <gashi.eduard@gmx.de>
2025-11-22 15:51:22 +0100

    Create Stripe Endpoint

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ import productRoutes from "./routes/product.js";
 import paymentRoutes from "./routes/order.js";
 import orderRoutes from "./routes/order.js";
 import userRoutes from "./routes/user.js";
+import stripeRoutes from "./routes/stripe.js"
 
 dotenv.config();
 
@@ -17,6 +18,7 @@ app.use("/api/products", productRoutes);
 app.use("/api/payment", paymentRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/stripe", stripeRoutes);
 
 
 const PORT = process.env.PORT || 5000;
