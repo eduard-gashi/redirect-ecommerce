6cc4fc0b2ac317506cfa164c799569eacbee9ea5
Eduard Gashi <gashi.eduard@gmx.de>
2025-12-20 13:45:25 +0100

    Add swiper/react dependencies

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default {
+export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['swiper']
+  },
   server: {
     proxy: {
-      '/api': 'http://localhost:5000', // or wherever your backend runs
+      '/api': 'http://localhost:5000',
     },
   },
-};
+});
